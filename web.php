8b226cc3feee639307fd1035c38943dced1d64ef
danieljacquin <58535425+danieljacquin@users.noreply.github.com>
2020-03-04 08:27:28 -0500

    Create web.php

---
+++ web.php
@@ -0,0 +1,13 @@
+<?php
+    class Persona{
+
+        public $nombre = 'daniel';
+       
+        public function Hablar($mensaje){
+         echo  "$mensaje";
+        }
+
+    }
+    $persona = new Persona();
+    $persona->Hablar("whats your name and last name");
+?>
